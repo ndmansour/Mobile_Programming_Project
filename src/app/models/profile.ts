6624f85bd4051c3f7a12f5efb44696e60df95843
NDIAYE <mndiaye@et.esiea.fr>
2018-12-14 15:03:46 +0100

    first commit

--- src/app/models/profile.ts
+++ src/app/models/profile.ts
@@ -2,6 +2,7 @@ export interface Profile  {
     first_name: string;
     last_name: string;
     age: string;
+    gender: string;
     address: string;
     phone: number;
     code_postal: number;
